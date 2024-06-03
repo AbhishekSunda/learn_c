53f9801cbfd63bd766ce24bb1399ef5f90a49d41
AjaySunda <aajaysunda@gmail.com>
2024-06-03 06:51:51 +0530

    initial commit

--- Input_output_d_types/Q1.c
+++ Input_output_d_types/Q1.c
@@ -1,1 +1,10 @@
 //Write a program to read an integer from the user and print it.
+
+#include<stdio.h>
+int main(){
+    int i;
+    printf("The value of integer is %d");
+    scanf("%d", &i);
+
+return 0;
+}

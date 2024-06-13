5b96a5103b3953e70078be90cb3f97b24ac804df
AjaySunda <aajaysunda@gmail.com>
2024-06-13 14:17:59 +0530

    solve q3

--- Input_output_d_types/Q3.c
+++ Input_output_d_types/Q3.c
@@ -1,6 +1,6 @@
 // Write a program to read a character from the user and print its ASCII value.
 
-#include<stdio.h>
+#include<stdio.h> // for input output declarations.
 int main(){
     char c;
     printf("Enter a character: ");

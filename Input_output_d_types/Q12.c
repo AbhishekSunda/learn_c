e50d8aeecaebf4d5d69f921ca4c546baa3cd4294
AjaySunda <aajaysunda@gmail.com>
2024-06-14 12:03:43 +0530

    solved Q12

--- Input_output_d_types/Q12.c
+++ Input_output_d_types/Q12.c
@@ -1,1 +1,16 @@
 // Write a program to read two integers and print their product, quotient (as a float), and remainder.
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+    printf("Enter value of b: ");
+    scanf("%d",&b);
+
+    printf("Product of a and b is %d\n", a*b);
+    printf("Quotient of a and b is %f\n", (float)a/b);
+    printf("Remainder of a and b is %d\n", a%b);
+
+return 0;
+}

4ade12638cb4f978bfd060c83fc8ed85f241a64c
AjaySunda <aajaysunda@gmail.com>
2024-06-15 11:51:48 +0530

    solved Q4

--- Input_output_d_types/Q4.c
+++ Input_output_d_types/Q4.c
@@ -1,5 +1,15 @@
 // Write a program to read a string from the user and print it.
 
+#include<stdio.h>
+int main(){
+    char string[50];
+    printf("What is your name?: ");
+    scanf("%[^\n]%*c", string);
+
+    printf("My name is %s.\n", string);
+return 0;
+}
+
 
 
 

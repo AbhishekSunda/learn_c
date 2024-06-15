9180708c0bbc236a244c169f6085e02646c3cea6
AjaySunda <aajaysunda@gmail.com>
2024-06-15 11:20:02 +0530

    solved Q2

--- Input_output_d_types/Q2.c
+++ Input_output_d_types/Q2.c
@@ -1,2 +1,11 @@
 // Write a program to read a floating-point number from the user and print it with two decimal places.
 
+#include<stdio.h>
+int main(){
+    float f;
+    printf("Enter a floating point number: ");
+    scanf("%f", &f);
+
+    printf("Entered floating-point number = %.2f\n", f);
+return 0;
+}

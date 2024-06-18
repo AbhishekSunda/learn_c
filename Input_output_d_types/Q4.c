0ac5ce481f245c5840f4fb3780c71ec9be24ff51
AjaySunda <aajaysunda@gmail.com>
2024-06-18 10:37:17 +0530

    added comment

--- Input_output_d_types/Q4.c
+++ Input_output_d_types/Q4.c
@@ -4,7 +4,7 @@
 int main(){
     char string[50];
     printf("What is your name?: ");
-    scanf("%[^\n]%*c", string);
+    scanf("%[^\n]%*c", string);//format specifier used for reading string input which discard a new line character \n. String will be stored in a variable declared string.
 
     printf("My name is %s.\n", string);
 return 0;

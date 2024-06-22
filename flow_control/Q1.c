4bdfd56fd3284862e6eb17458b5805840cfe9528
AjaySunda <aajaysunda@gmail.com>
2024-06-22 21:35:54 +0530

    solve1

--- flow_control/Q1.c
+++ flow_control/Q1.c
@@ -1,1 +1,14 @@
 // Write a program to read an integer and a character, and print their sum after converting the character to its integer ASCII value.
+#include<stdio.h>
+int main(){
+    int i;
+    char c;
+    printf("Enter an integer value\n");
+    scanf("%d", &i);
+    printf("Enter a character\n");
+    scanf(" %c", &c);
+    printf("The ASCII value of %c is %d.\n", c, c);
+    int sum = i + c;
+    printf("Sum of integer and ASCII value of character = %d\n", sum);
+return 0;
+}

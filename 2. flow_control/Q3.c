40e50e718da4c0ce214bc185f29fddbfa63c74c9
AjaySunda <aajaysunda@gmail.com>
2024-06-23 08:02:46 +0530

    solve3

--- 2. flow_control/Q3.c
+++ 2. flow_control/Q3.c
@@ -1,1 +1,11 @@
 // Write a program to read an integer, cast it to a character, and print both the original integer and the resulting character.
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Enter an integer value: ");
+    scanf("%d", &i);
+    char c = i;
+    printf("Entered integer value = %d\n", i);
+    printf("Character of casted integer value = %c\n", c);
+return 0;
+}

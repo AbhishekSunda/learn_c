654a0e0fea6836354e8822f092b343212d3e7308
AjaySunda <aajaysunda@gmail.com>
2024-06-23 08:42:47 +0530

    solve4

--- 2. flow_control/Q4.c
+++ 2. flow_control/Q4.c
@@ -1,1 +1,15 @@
 // Write a program to read two floating-point numbers, cast them to integers, and then print their product as an integer.
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("Enter first floating-point number: ");
+    scanf("%f", &a);
+    printf("Enter second floating-point number: ");
+    scanf("%f", &b);
+
+    int x = (int)a;
+    int y = (int)b;
+    int product = x * y;
+    printf("Product=%d\n", product);
+return 0;
+}

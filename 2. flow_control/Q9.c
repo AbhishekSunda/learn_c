d55bf39d8b79f2bc4f261598f0dabdce65b2c960
AjaySunda <aajaysunda@gmail.com>
2024-06-23 12:13:52 +0530

    solve9

--- 2. flow_control/Q9.c
+++ 2. flow_control/Q9.c
@@ -1,1 +1,18 @@
 // Write a program to read an integer and print whether it is even or odd using a switch statement.
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Enter an integer value\n");
+    scanf("%d", &i);
+    
+    switch (i%2==0)
+    {
+    case 1:
+        printf("It is even.\n");
+        break;
+    
+    default: printf("It is odd.\n");
+        break;
+    }
+return 0;
+}

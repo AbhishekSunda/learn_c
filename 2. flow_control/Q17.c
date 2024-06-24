3b7b8deff4e76e4b0b13bec09e8f9e100c1303af
AjaySunda <aajaysunda@gmail.com>
2024-06-24 18:44:58 +0530

    solve17

--- 2. flow_control/Q17.c
+++ 2. flow_control/Q17.c
@@ -1,1 +1,30 @@
 // Write a program to read an array of n integers and find the maximum and minimum values in the array using a for loop.
+#include<stdio.h>
+int main(){
+    int array[10],n,i,maximum,minimum;
+    printf("Number of elements in the array = ");
+    scanf("%d", &n);
+
+    for ( i = 0; i < n; i++)
+    {
+        printf("Enter values in the array : ");
+        scanf("%d", &array[i]);
+    }
+    maximum = array[0];
+    minimum = array[0];
+    for ( i = 0; i < n; i++)
+    {
+        if(array[i]>maximum)
+        {
+           maximum = array[i];
+        }
+        if (array[i]<minimum)
+        {
+           minimum = array[i];
+        }   
+    }
+        printf("Maximum value = %d\n", maximum);
+        printf("Minimum value = %d\n", minimum);
+    
+return 0; 
+}

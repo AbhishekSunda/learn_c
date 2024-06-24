d830ba24278e8e3825bc4c3b35fa91a526a8ccd0
AjaySunda <aajaysunda@gmail.com>
2024-06-24 10:57:03 +0530

    case 1 -> case true

--- 2. flow_control/Q9.c
+++ 2. flow_control/Q9.c
@@ -1,5 +1,7 @@
 // Write a program to read an integer and print whether it is even or odd using a switch statement.
 #include<stdio.h>
+#include<stdbool.h>
+
 int main(){
     int i;
     printf("Enter an integer value\n");
@@ -7,11 +9,11 @@ int main(){
     
     switch (i%2==0)
     {
-    case 1:
-        printf("It is even.\n");
+    case true:
+        printf("It is an even number.\n");
         break;
     
-    default: printf("It is odd.\n");
+    default: printf("It is an odd number.\n");
         break;
     }
 return 0;

dd26bf0afd28d8ae0846f5e4a0ba6bc516ce5968
AjaySunda <aajaysunda@gmail.com>
2024-06-25 17:24:33 +0530

    :

--- 2. flow_control/Q15.c
+++ 2. flow_control/Q15.c
@@ -1,11 +1,11 @@
 // Write a program to read an array of n integers and print the array in reverse order using a for loop.
 #include<stdio.h>
 int main(){
-    int n,array[100],i;
+    int n,array[10],i;
     printf("Enter the number of elements in the array = ");
     scanf("%d", &n);
     
-    for ( i = 0; i < n; i++) 
+    for ( i = 0; i < n-1; i++) 
     {
         printf("Enter integer value : ");
         scanf("%d", &array[i]);
--- 2. flow_control/Q23.c
+++ 2. flow_control/Q23.c
@@ -1,1 +1,17 @@
 // Write a program to read an integer and print its binary representation using a while loop. also read about binary representation
+#include<stdio.h>
+int main(){
+    int n,remainder,binary=0,digit=0;
+    printf("Enter an integer number\n");
+    scanf("%d", &n);
+
+    while (n>0)
+    {
+        remainder = n % 2;
+                n = n / 2;
+           binary = binary + remainder * digit;
+            digit = digit * 10;
+    }
+    printf("Binary representation: %d", digit);
+return 0;
+}

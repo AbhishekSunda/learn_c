48483f9e216659d0b2d02b71005fa6cdb42f3993
AjaySunda <aajaysunda@gmail.com>
2024-07-01 11:41:48 +0530

    solvedQ12

--- 2. flow_control/Q12.c
+++ 2. flow_control/Q12.c
@@ -1,1 +1,18 @@
 // Write a program to read an integer and print whether it is positive, negative, or zero using nested if-else statements.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter an integer number : ");
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        printf("Entered number is positive.\n");
+    }
+    else if(n < 0)
+    {
+        printf("Entered number is negative.\n");
+    }
+    else printf("Entered number is zero.\n");
+return 0;
+}
--- 2. flow_control/Q15.c
+++ 2. flow_control/Q15.c
@@ -1,1 +1,26 @@
 // Write a program to read an array of n integers and print the array in reverse order using a for loop.
+#include<stdio.h>
+int main(){
+    int n,array[10],i;
+    printf("Enter the number of elements in the array = ");
+    scanf("%d", &n);
+    
+    for ( i = 0; i <= n-1; i++) //n-1 because array start from 0 and goes till n-1
+    {
+        printf("Enter integer value : ");
+        scanf("%d", &array[i]);
+    }
+    printf("array : ");
+    for ( i = 0; i <= n-1; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\nreverse array : ");
+    for ( i = n-1; i >= 0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+    
+return 0;
+}

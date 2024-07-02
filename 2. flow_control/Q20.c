6d9b892deb1982928320df22b572cc3980204e12
AjaySunda <aajaysunda@gmail.com>
2024-07-02 10:53:54 +0530

    SolvedQ20

--- 2. flow_control/Q20.c
+++ 2. flow_control/Q20.c
@@ -1,1 +1,26 @@
 // Write a program to read integers from the user until a negative number is entered. Print the sum of all entered positive integers using a while loop.
+#include<stdio.h>
+int main(){
+    int value,i=1,sum=0;
+
+    while (1)
+    {
+        printf("Enter the value\n");
+        scanf("%d", &value);
+
+        if (value < 0)
+            {
+                break;
+            }
+        if (value > 0)
+        {
+            sum += value; 
+
+        }
+
+        i++;
+    }
+        printf("Sum of all positive integers until a negative integer is entered = %d\n", sum);
+
+ return 0;   
+}

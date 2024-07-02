1006af50c43c450cbf8151b3d235b98d67dadf78
AjaySunda <aajaysunda@gmail.com>
2024-07-02 16:41:17 +0530

    SolvedQ15

--- 2. flow_control/Q15.c
+++ 2. flow_control/Q15.c
@@ -1,1 +1,26 @@
 // Write a program to read an array of n integers and print the array in reverse order using a for loop.
+#include<stdio.h>
+int main(){
+    int n,array[100],i;
+    printf("Enter the number of elements in the array = ");
+    scanf("%d", &n);
+
+    for ( i = 0; i <= n-1; i++) 
+    {
+        printf("Enter integer value : ");
+        scanf("%d", &array[i]);
+    }
+    printf("array : ");
+    for ( i = 0; i <= n-1; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\nreverse array : ");
+    for ( i = n-1; i >= 0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+
+return 0;
+}

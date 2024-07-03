ac2e7cd5b64763e4b00106f045656959cde20014
AjaySunda <aajaysunda@gmail.com>
2024-07-03 11:09:17 +0530

    Using array[n] and n in place of n-1

--- 2. flow_control/Q15.c
+++ 2. flow_control/Q15.c
@@ -1,23 +1,24 @@
 // Write a program to read an array of n integers and print the array in reverse order using a for loop.
 #include<stdio.h>
 int main(){
-    int n,array[100],i;
+    int n;
     printf("Enter the number of elements in the array = ");
     scanf("%d", &n);
+    int array[n];
 
-    for ( i = 0; i <= n-1; i++) 
+    for ( int i = 0; i < n; i++) 
     {
         printf("Enter integer value : ");
         scanf("%d", &array[i]);
     }
     printf("array : ");
-    for ( i = 0; i <= n-1; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", array[i]);
     }
 
     printf("\nreverse array : ");
-    for ( i = n-1; i >= 0; i--)
+    for (int i = n-1; i >= 0; i--)
     {
         printf("%d ", array[i]);
     }

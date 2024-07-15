d1b33f05b82654b36e2a9c965ab9f227d2f706d4
AjaySunda <aajaysunda@gmail.com>
2024-07-15 19:33:11 +0530

    small change in Q25

--- 2. flow_control/Q25.c
+++ 2. flow_control/Q25.c
@@ -3,22 +3,22 @@
 int main(){
     int vector1[3];
     int vector2[3];
-    int i,j,dot_product=0;
+    int dot_product = 0;
 
-    for ( i = 0; i < 3; i++)
-        {
-            printf("Enter value for vector1 %d: ", i);
-            scanf("%d", &vector1[i]);
-        }
-        
-    for ( i = 0; i < 3; i++)
-        {
-            printf("Enter value for vector2 %d: ", i);
-            scanf("%d", &vector2[i]);
-        }
-    for (i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter value for vector1 %d: ", i);
+        scanf("%d", &vector1[i]);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter value for vector2 %d: ", i);
+        scanf("%d", &vector2[i]);
+    }
+    for (int i = 0; i < 3; i++) {
         dot_product += vector1[i] * vector2[i];
     }
-     printf("The dot product of the two vectors = %d\n", dot_product);
-return 0;
+    printf("The dot product of the two vectors = %d\n", dot_product);
+    return 0;
 }

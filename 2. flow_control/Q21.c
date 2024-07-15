c2027cfb56fbf7c68d42f5e941fc78d921b4ffcb
AjaySunda <aajaysunda@gmail.com>
2024-07-15 19:24:26 +0530

    small change in Q21

--- 2. flow_control/Q21.c
+++ 2. flow_control/Q21.c
@@ -1,21 +1,21 @@
 // Write a program to read an integer n and check if it is a prime number
 #include<stdio.h>
 int main(){
-    int n,i,z=0;
+    int n, z = 0;
     printf("Enter a number : ");
     scanf("%d", &n);
 
-    for ( i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (n % i == 0)
-           z++;    
+            z++;
     }
-    if (z==2)
+    if (z == 2)
     {
         printf("%d is a prime number,\n", n);
     }
     else{
-         printf("%d is not a prime number.\n", n);
-    }   
-return 0;    
+        printf("%d is not a prime number.\n", n);
+    }
+    return 0;
 }

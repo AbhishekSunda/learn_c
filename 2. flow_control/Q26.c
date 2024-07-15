e41d00519a0ad615bbdad4547a9e6dfcde771b0d
AjaySunda <aajaysunda@gmail.com>
2024-07-15 19:35:14 +0530

    small change in Q26

--- 2. flow_control/Q26.c
+++ 2. flow_control/Q26.c
@@ -3,37 +3,37 @@
 //If we take transpose of the matrix, rows and columns interchange. 
 #include<stdio.h>
 int main(){
-    int m,n,i,j;
-     printf("Enter no. of rows = ");
+    int m, n;
+    printf("Enter no. of rows = ");
     scanf("%d", &m);
     printf("Enter no. of columns = ");
     scanf("%d", &n);
     int array[m][n];
-    for ( i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
-        for ( j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
-            printf("Enter elements at %d%d : ", i,j);
+            printf("Enter elements at %d%d : ", i, j);
             scanf("%d", &array[i][j]);
         }
     }
     printf("\nMatrix : \n");
-    for ( i = 0; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
-        for ( j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("%d ", array[i][j]);
         }
-         printf("\n");
+        printf("\n");
     }
     printf("\nTranspose matrix : \n");
-    for ( i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for ( j = 0; j < m; j++)
+        for (int j = 0; j < m; j++)
         {
             printf("%d ", array[j][i]);
         }
         printf("\n");
     }
-return 0;
+    return 0;
 }

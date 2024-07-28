dd8f17c1f51be8a5c69ddcc57cfdd7727f1c6a15
AjaySunda <aajaysunda@gmail.com>
2024-07-28 20:04:18 +0530

    solved functions Q5

--- 3.functions/Q5.c
+++ 3.functions/Q5.c
@@ -5,3 +5,27 @@
     // Example:\n
     // Input: 3, 7, 5\n
     // Output: "The maximum number is 7."\n
+
+#include<stdio.h>
+
+int maxOfThree(int a, int b, int c){
+    if (a > b && a > c)
+        return a;
+    if (b > a && b > c)
+        return b;
+    if (c > a && c > b)
+        return c;
+}
+
+int main(){
+    int x, y, z;
+    printf("Enter value of x : ");
+    scanf("%d", &x);
+    printf("Enter value of y : ");
+    scanf("%d", &y);
+    printf("Enter value of z : ");
+    scanf("%d", &z);
+
+    printf("Maximum between %d, %d and %d is %d\n", x, y, z, maxOfThree(x, y, z));
+    return 0;
+}

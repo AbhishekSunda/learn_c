7505725aec3548fac7bf0bd8024d8ec387b974f4
AjaySunda <aajaysunda@gmail.com>
2024-07-28 16:51:20 +0530

    bool data type and removed i

--- 2. flow_control/Q20.c
+++ 2. flow_control/Q20.c
@@ -1,26 +1,25 @@
 // Write a program to read integers from the user until a negative number is entered. Print the sum of all entered positive integers using a while loop.
 #include<stdio.h>
+#include<stdbool.h>
 int main(){
-    int value,i=1,sum=0;
+    int value, sum = 0;
 
-    while (1)
+    while (true)
     {
         printf("Enter the value\n");
         scanf("%d", &value);
 
         if (value < 0)
-            {
-                break;
-            }
+        {
+            break;
+        }
         if (value > 0)
         {
-            sum += value; 
+            sum += value;
 
         }
-
-        i++;
     }
-        printf("Sum of all positive integers until a negative integer is entered = %d\n", sum);
+    printf("Sum of all positive integers until a negative integer is entered = %d\n", sum);
 
- return 0;   
+    return 0;
 }

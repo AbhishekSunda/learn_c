6a38062dbf6cc4f0bb11059829626f2f9f0539b5
AjaySunda <aajaysunda@gmail.com>
2024-07-28 22:23:19 +0530

    again solved functions q3

--- 3.functions/Q3.c
+++ 3.functions/Q3.c
@@ -14,12 +14,35 @@ float celsiusToFahrenheit(float celsius){
     Fahrenheit = (float)(celsius * 9 / 5) + 32;
     return Fahrenheit;
 }
+float farhenheitToCelsius(float fahrenheit){
+    // C = (5/9)F - 32.
+    float Celsius;
+    Celsius = (float)5 / 9 * (fahrenheit - 32);
+    return Celsius;
+}
 
 int main(){
-    float degreeCelsius;
-    printf("Enter temperature in degree Celsius : ");
-    scanf("%f", &degreeCelsius);
+    int choice;
+    printf("Enter 1 for converting Celsius into Fahrenheit.\n");
+    printf("Enter 2 for converting Fahrenheit into Celsius.\n");
+    printf("Choose 1 or 2 : ");
+    scanf("%d", &choice);
 
-    printf("Temperature in degree Fahrenheit = %.1f\n", celsiusToFahrenheit(degreeCelsius));
+    if (choice == 1){
+        float degreeCelsius;
+        printf("Enter temperature in degree Celsius : ");
+        scanf("%f", &degreeCelsius);
+
+        printf("Temperature in degree Fahrenheit = %.1f\n", celsiusToFahrenheit(degreeCelsius));
+    }
+    if (choice == 2)
+    {
+        float degreeFahrenheit;
+        printf("Enter temperature in degree Fahrenheit : ");
+        scanf("%f", &degreeFahrenheit);
+
+        printf("Temperature in degree Celsius = %.1f\n", farhenheitToCelsius(degreeFahrenheit));
+    }
+
     return 0;
 }
